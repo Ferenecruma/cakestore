e7087c76d56ca28cb5e6a446c4e137de40a084e8
Roman <ferenecruma@gmail.com>
2020-09-09 01:06:10 +0300

    added fetch requests for shopping cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from webstore.models import Product
+from .cart import Cart
 
-# Create your views here.
+
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product)
+    response_data = {
+                    'status' : 'success', 
+                    'message' : 'added',
+                    'items_num' : str(len(cart))}
+    return JsonResponse(response_data)
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product=product)
+    response_data = {'status' : 'success', 'message' : 'removed'}
+    return JsonResponse(response_data)
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
